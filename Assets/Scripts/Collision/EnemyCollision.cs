0d7e5c20b020731c25552c00f34f3fa6e8df53d7
Quang Bui <johndamien9999@gmail.com>
2024-10-25 23:10:40 -0400

    Update enemy attack animation when they come close to the player

--- Assets/Scripts/Collision/EnemyCollision.cs
+++ Assets/Scripts/Collision/EnemyCollision.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour
 {
     [SerializeField] GameObject _gun;
+    [SerializeField] Animator _enemyAnimator;
+
     WeaponStat _weaponStat;
     EnemyStats _enemyStat;
-    
     float _gunDamage;
 
     void Start()
@@ -23,6 +24,22 @@ public class EnemyCollision : MonoBehaviour
         {
             Destroy(collision.gameObject);
             _enemyStat.TakeDamage(_gunDamage);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _enemyAnimator.SetBool("isZombieAttack", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _enemyAnimator.SetBool("isZombieAttack", false);
         }
     }
 }
