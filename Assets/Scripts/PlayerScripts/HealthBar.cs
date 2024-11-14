88cd5413c9042458e5b85179259994d9326352b3
Quang Bui <johndamien9999@gmail.com>
2024-11-13 23:56:21 -0500

    add bosses for each map

--- Assets/Scripts/PlayerScripts/HealthBar.cs
+++ Assets/Scripts/PlayerScripts/HealthBar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class HealthBar : MonoBehaviour
+public class HealthBar1 : MonoBehaviour
 {
     public Slider healthBar;
     public PlayerStats stats;
